122e80398369b3d42490ea62bc67f4673f97728f
[Digvikadam] <[digvikadam168@gmail.com]>
2021-10-31 23:41:27 +0530

    Added: Check Employee Daily Wage

--- com/bridgelabz/EmployeeWage.java
+++ com/bridgelabz/EmployeeWage.java
@@ -3,15 +3,21 @@ package com.bridgelabz;
 import java.util.Random;
 public class EmployeeWage {
 	public static void main(String[] args) {
-		final int IS_PRESENT = 1;
+		int IS_PRESENT = 1;
+		int RATE_PER_HR = 20;
+		int emphrs = 0;
+		int empwage = 0;
 		Random r = new Random();
 		int empcheck = r.nextInt(2);
-		
 		if(empcheck==IS_PRESENT) {
 			System.out.println("Employee is Present");
+			emphrs=8;
 		}
 		else {
 			System.out.println("Employee is Absent");
+			emphrs=0;
 		}
-	}	
+		empwage = emphrs * RATE_PER_HR;
+		System.out.println("empwage:" +empwage);
+	}
 }	
