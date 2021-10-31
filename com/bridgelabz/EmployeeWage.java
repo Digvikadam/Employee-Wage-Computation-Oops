ade9779df3414bbacbeb33054229a773cdd57a98
[Digvikadam] <[digvikadam168@gmail.com]>
2021-10-31 23:51:21 +0530

    Added: Check Employee Wage by using Case Statement

--- com/bridgelabz/EmployeeWage.java
+++ com/bridgelabz/EmployeeWage.java
@@ -2,29 +2,31 @@ package com.bridgelabz;
 
 import java.util.Random;
 public class EmployeeWage {
-	public static void main(String[] args) {
-		
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int RATE_PER_HR = 20;
-		int emphrs = 0;
+	public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+	
+    public static void main(String[] args) {
 		int empwage = 0;
+		int emphrs = 0;
 		Random r = new Random();
 		int empcheck = r.nextInt(3);
-		if(empcheck==IS_FULL_TIME) {
+		
+		switch(empcheck) {
+		case IS_PART_TIME:
+			System.out.println("Employee is Present");
+			emphrs = 4;
+		break;
+		
+		case IS_FULL_TIME:
 			System.out.println("Employee is Present");
 			emphrs = 8;
-		}
-		else if(empcheck==IS_PART_TIME){
-			System.out.println("Employee is Present ");
-			emphrs = 4;	
-		}
-		else {
+			break;
+		default:
 			System.out.println("Employee is Absent");
 			emphrs = 0;
 		}
-		empwage = emphrs * RATE_PER_HR;
+		empwage = emphrs * EMP_RATE_PER_HOUR;
 		System.out.println("empwage:" +empwage);
-		
 	}
 }	
