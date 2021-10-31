33ea22d7321cb513c56f9dfe4d2bfe7e4782f416
[Digvikadam] <[digvikadam168@gmail.com]>
2021-10-31 23:46:04 +0530

    Added: Check Employee Part Time Wage

--- com/bridgelabz/EmployeeWage.java
+++ com/bridgelabz/EmployeeWage.java
@@ -3,21 +3,28 @@ package com.bridgelabz;
 import java.util.Random;
 public class EmployeeWage {
 	public static void main(String[] args) {
-		int IS_PRESENT = 1;
+		
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int RATE_PER_HR = 20;
 		int emphrs = 0;
 		int empwage = 0;
 		Random r = new Random();
-		int empcheck = r.nextInt(2);
-		if(empcheck==IS_PRESENT) {
+		int empcheck = r.nextInt(3);
+		if(empcheck==IS_FULL_TIME) {
 			System.out.println("Employee is Present");
-			emphrs=8;
+			emphrs = 8;
+		}
+		else if(empcheck==IS_PART_TIME){
+			System.out.println("Employee is Present ");
+			emphrs = 4;	
 		}
 		else {
 			System.out.println("Employee is Absent");
-			emphrs=0;
+			emphrs = 0;
 		}
 		empwage = emphrs * RATE_PER_HR;
 		System.out.println("empwage:" +empwage);
+		
 	}
 }	
