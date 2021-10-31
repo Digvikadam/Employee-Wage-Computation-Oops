a06b3d72ce51100878cb654665c864055ecdd1a3
[Digvikadam] <[digvikadam168@gmail.com]>
2021-10-31 23:58:11 +0530

    Added: Monthly Wage Of Employee

--- com/bridgelabz/EmployeeWage.java
+++ com/bridgelabz/EmployeeWage.java
@@ -5,21 +5,22 @@ public class EmployeeWage {
 	public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
-	
+    public static final int NUM_OF_WORKING_DAYS = 20;
+    
     public static void main(String[] args) {
 		int empwage = 0;
 		int emphrs = 0;
+		int totalEmpWage = 0;
+		for (int day = 0; day <  NUM_OF_WORKING_DAYS; day++) {
 		Random r = new Random();
 		int empcheck = r.nextInt(3);
-		
 		switch(empcheck) {
 		case IS_PART_TIME:
 			System.out.println("Employee is Present");
 			emphrs = 4;
-		break;
-		
+			break;
 		case IS_FULL_TIME:
-			System.out.println("Employee is Present");
+			System.out.println("Employee Is Present");
 			emphrs = 8;
 			break;
 		default:
@@ -27,6 +28,9 @@ public class EmployeeWage {
 			emphrs = 0;
 		}
 		empwage = emphrs * EMP_RATE_PER_HOUR;
-		System.out.println("empwage:" +empwage);
-	}
+		totalEmpWage += empwage;
+      		System.out.println("Emp Wage : " + empwage);
+	    	}
+		System.out.println("Total Emp Wage : " + totalEmpWage);
+    }	
 }	
